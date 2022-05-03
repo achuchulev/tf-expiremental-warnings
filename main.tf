ea90e0c35ff00c38f6143cc9dcb83a6f6a8e6715
Atanas Chuchulev <atanas.v4@gmail.com>
2022-05-03 18:16:57 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,31 @@
-module "foo" {
+module "1" {
     source = "./modules/null"
 }
 
-module "bar" {
+module "2" {
+    source = "./modules/null"
+}
+
+module "3" {
+    source = "./modules/null"
+}
+
+module "4" {
+    source = "./modules/null"
+}
+
+module "5" {
+    source = "./modules/null"
+}
+
+module "6" {
+    source = "./modules/null"
+}
+
+module "7" {
+    source = "./modules/null"
+}
+
+module "8" {
     source = "./modules/null"
 }
